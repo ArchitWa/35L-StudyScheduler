311d7e5c6798cfebf5fea6ae25d1ca43f35195ce
Leo Sun <leobluehills2015@gmail.com>
2026-02-27 14:59:38 -0800

    reverted changes

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,27 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 
 const app = express();
+dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGIN || true,
+    credentials: false,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 3000;
