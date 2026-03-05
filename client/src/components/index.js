29b70b3dcfa88cf0cecd286f163f4da0cabd73e9
Jonathan Liu <jonathanhliu21@gmail.com>
2026-03-04 23:41:40 -0800

    fix import error in index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,6 @@
 export { default as Navbar } from "./navbar.jsx";
 export { default as CreateGroupModal } from "./groups/CreateGroupModal.jsx";
 export { default as GroupModal } from "./groups/GroupModal.jsx";
-export { default as NewGroupCard } from "./groups/NewGroupCard.jsx";
 export { default as ClassPill } from "./ClassPill.jsx";
 export { default as ProfileEditorModal } from "./ProfileEditorModal.jsx";
 export { default as ProfileCreatorModal } from "./ProfileCreatorModal.jsx";
