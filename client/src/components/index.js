128cf5c1b41becf7af062b26f0dcb1dec203c2f5
Archit Wankhade <architw@g.ucla.edu>
2026-03-04 21:16:55 -0800

    Added in editing, removed creation testing dashboard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Navbar } from "./navbar.jsx";
 export { default as CreateGroupModal } from "./groups/CreateGroupModal.jsx";
 export { default as GroupModal } from "./groups/GroupModal.jsx";
-export { default as NewGroupCard } from "./groups/NewGroupCard.jsx";
+export { default as EditGroupModal } from "./groups/EditGroupModal.jsx";
 export { default as ClassPill } from "./ClassPill.jsx";
